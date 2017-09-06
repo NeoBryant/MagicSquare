29c16dd88228899a33c008783424fba9bb6d250e
NeoBryant <1512917808@qq.com>
2017-09-06 17:40:48 +0800

    first commit

---
+++ MagicSquare/Square.h
@@ -0,0 +1,13 @@
+//
+//  Square.h
+//  MagicSquare
+//
+//  Created by 彭 on 2017/9/6.
+//  Copyright © 2017年 彭. All rights reserved.
+//
+
+#ifndef Square_h
+#define Square_h
+
+
+#endif /* Square_h */
